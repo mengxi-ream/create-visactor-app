e39979ef0d4205735d050f8f3e59c0743546424c
Kuiliang Zhang <z1219202167@outlook.com>
2025-02-25 23:58:07 -0800

    docs: update doc site

--- www/src/lib/source.ts
+++ www/src/lib/source.ts
@@ -1,7 +1,15 @@
 import { docs } from "@/.source";
 import { loader } from "fumadocs-core/source";
+import { createElement } from "react";
+import { icons } from "lucide-react";
 
 export const source = loader({
   baseUrl: "/docs",
   source: docs.toFumadocsSource(),
+
+  icon(icon) {
+    if (icon && icon in icons)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      return createElement(icons[icon as keyof typeof icons]);
+  },
 });
