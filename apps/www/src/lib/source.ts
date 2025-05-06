86534184960aec43191c80c8758243a0765cbca1
Kuiliang Zhang <z1219202167@outlook.com>
2025-05-06 16:17:40 -0700

    chore: update doc website index page

--- apps/www/src/lib/source.ts
+++ apps/www/src/lib/source.ts
@@ -4,7 +4,7 @@ import { icons } from "lucide-react";
 import { createElement } from "react";
 
 export const source = loader({
-  baseUrl: "/docs",
+  baseUrl: "/",
   source: docs.toFumadocsSource(),
 
   icon(icon) {
